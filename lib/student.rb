994fe1ee1b147f5390aec992646b335b1af46c6d
Dakota States <dakota.states@gmail.com>
2020-03-13 19:27:37 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,19 +6,26 @@ class Student
   #  with DB[:conn]
 
   def initialize(name, grade, id = nil)
-    @name = name
-    @grade = grade
-    @id = id
+    @name = name 
+    @grade = grade 
+    @id = id 
   end
   
-  def self.create_table 
-    sql = <<-SQL 
+  def self.create_table
+    sql = <<-SQL
     CREATE TABLE IF NOT EXISTS students (
     id INTEGER PRIMARY KEY,
     name TEXT,
-    grade INTEGER
-    )
+    grade INTEGER)
     SQL
     DB[:conn].execute(sql)
   end
+  
+  def self.drop_table
+    sql = <<-SQL
+    DROP TABLE students
+    SQL
+    DB[:conn].execute(sql)
+  end
+
 end
