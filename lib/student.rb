64fd50c7ef96a89d8ec72e8de6819451f9c3c827
Dakota States <dakota.states@gmail.com>
2020-03-13 20:14:45 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -57,10 +57,10 @@ end
  
   def self.new_from_db(row)
     
-    new_student.id row[0]
-    new_student.name = row[1]
-    new_student.grade = row[2]
-     self.new(id, name, grade)
+    id row[0]
+    name = row[1]
+    grade = row[2]
+    self.new(id, name, grade)
   end
   
   def self.find_by_name(name)
