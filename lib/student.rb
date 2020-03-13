f6c1e9d40e0c4f2f50676f92f0fe7496448190b0
Dakota States <dakota.states@gmail.com>
2020-03-13 19:49:52 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -58,5 +58,13 @@ attr_reader :id
     new_student
   end
   
-  
+  def self.find_by_name(name)
+    sql = <<-SQL
+    SELECT * FROM students WHERE name = ?
+    Limit 1
+    SQL
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
 end
